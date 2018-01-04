d2438415a78570bb44325edb84a00495c076b510
rodrigodavid <rodrigo.monney@gmail.com>
2018-01-03 22:24:22 -0200

    Code of the tutorial #10 - Creating / Removing Directories

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 var fs = require('fs');
 
-// reading a file synchronously
-// first: name of the file;
-// second: encoding.
-var readme = fs.readFileSync('readme.txt', 'utf8');
+// delete a file using fs
+// fs.unlink('writeme.txt');
 
-// writing a file synchronously
-// first: name of the file;
-// second: content of the file.
-fs.writeFileSync('writeme.txt', readme);
+// creating a directory synchronously
+// fs.mkdirSync('stuff');
 
-// reading a file asynchronously
-// this version don't block the code
-var readme = fs.readFile('readme.txt', 'utf8', function (err, data) {
-    console.log(data);
-    fs.writeFileSync('secondfile.txt', data);
-});
+// deleting a directory synchronously
+// fs.rmdirSync('stuff');
 
-console.log('This text will appear first than line 16');
+// creating a directory asynchronously
+// fs.mkdir('stuff', function () {
+//     fs.readFile('readme.txt', 'utf8', function (err, data ) {
+//         fs.writeFileSync('./stuff/writeme.txt', data);
+//     });
+// });
+
+// you can't just remove a directory, you have to remove the stuff inside it
+// fs.unlink('./stuff/writeme.txt', function () {
+//     fs.rmdirSync('stuff');
+// });
