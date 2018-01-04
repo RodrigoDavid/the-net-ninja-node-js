02dd9d278e1cd1958d741f2ea44996c173f1ca99
rodrigodavid <rodrigo.monney@gmail.com>
2018-01-04 20:40:48 -0200

    Code of the tutorial #14 - Readable Streams

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 // requiring the node http module
 var http = require('http');
+var fs = require('fs');
 
-// storing the server method in variable
-var server = http.createServer(function (req, res) {
-    // when a request is made by the client, this function is executed
-    // writing response headers
-    res.writeHead(200, {'Content-Type' : 'text/plain'});
+// creating a read stream and specifying which file to read
+var myReadStream = fs.createReadStream(__dirname + '/lorem.txt', 'utf8');
 
-    // log the request url to the console
-    console.log('request url: ' + req.url);
-
-    // end the response and send it to the browser
-    res.end('Sending just this plain text...');
+// filling the buffer
+myReadStream.on('data', function (chunk) {
+    console.log('new chunk received: ');
+    console.log(chunk);
 });
 
-// specifying a port to listen to
-server.listen(3000, '127.0.0.1');
-
-console.log('Listening on port 3000...');
+// // storing the server method in variable
+// var server = http.createServer(function (req, res) {
+//     // when a request is made by the client, this function is executed
+//     // writing response headers
+//     res.writeHead(200, {'Content-Type' : 'text/plain'});
+//
+//     // log the request url to the console
+//     console.log('request url: ' + req.url);
+//
+//     // end the response and send it to the browser
+//     res.end('Sending just this plain text...');
+// });
+//
+// // specifying a port to listen to
+// server.listen(3000, '127.0.0.1');
+//
+// console.log('Listening on port 3000...');
