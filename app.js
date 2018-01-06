4792bf4eed372e7e899298300219841ac535942b
rodrigodavid <rodrigo.monney@gmail.com>
2018-01-05 23:43:02 -0200

    Code of the tutorial #19 - Basic Routing

--- app.js
+++ app.js
@@ -4,18 +4,26 @@ var fs = require('fs');
 
 // storing the server method in variable
 var server = http.createServer(function (req, res) {
-    // when a request is made by the client, this function is executed
-    // writing response headers
-    res.writeHead(200, {'Content-Type': 'application/json'});
+    console.log('request was made: ' + req.url);
 
-    // creating an object to send to the client
-    var myAwesomeObject = {
-        name: 'James',
-        job: 'Ninja',
-        age: 27
-    };
+    // checking the request
+    if(req.url === '/home' || req.url === '/') {
+        res.writeHead(200, 'text/html');
 
-    res.end(JSON.stringify(myAwesomeObject));
+        fs.createReadStream(__dirname + '/index.html').pipe(res);
+    } else if(req.url === '/contact'){
+        res.writeHead(200, 'text/html');
+
+        fs.createReadStream(__dirname + '/contact.html').pipe(res);
+    } else if(req.url === '/api/ninjas') {
+        var ninjas = [{name: 'James', age: 29}, {name: 'Marx', age: 14}];
+
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(ninjas));
+    } else {
+        res.writeHead(200, 'text/html');
+        fs.createReadStream(__dirname + '/404.html').pipe(res);
+    }
 });
 
 // specifying a port to listen to
